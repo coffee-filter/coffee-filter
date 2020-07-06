afdb10f31a0371d9fc7a96c1af938230aedd12c7
Marco Muser <marco.muser@posteo.de>
2020-07-06 18:40:52 +0200

    add dom manipulation to wizard

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('.acidity-strength').style.display = "none";
+document.querySelector('.taste-characteristics').style.display = "none";
 
-  console.log('IronGenerator JS imported successfully!');
 
-}, false);
+const wizzardButtons = document.querySelectorAll('button');
+
+wizzardButtons[0].onclick = function() {
+  document.querySelector('.brewing-method').style.display = "none";
+  document.querySelector('.acidity-strength').style.display = "inherit";
+}
+
+wizzardButtons[1].onclick = function() {
+  document.querySelector('.acidity-strength').style.display = "none";
+  document.querySelector('.taste-characteristics').style.display = "inherit";
+}
