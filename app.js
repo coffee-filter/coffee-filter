03d907c4a424e7dc6cd4cad1d58890fac6756099
Marco Muser <marco.muser@posteo.de>
2020-07-06 15:24:25 +0200

    add wizard

--- app.js
+++ app.js
@@ -54,5 +54,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+const wizard = require('./routes/wizard');
+app.use('/', wizard);
+
 
 module.exports = app;
