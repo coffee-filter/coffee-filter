9490e1c1f909f33377dc475ee3f303677c4a2e4d
Marco Muser <marco.muser@posteo.de>
2020-07-07 15:48:17 +0200

    refactor wizard

--- routes/wizard.js
+++ routes/wizard.js
@@ -5,7 +5,7 @@ const Coffee = require('../models/Coffee');
 /* GET home page */
 router.get('/wizard', (req, res, next) => {
   res.render('wizard');
-});
+})
 
 router.get('/wizard/2', (req, res, next) => {
   res.render('wizard2');
@@ -16,7 +16,15 @@ router.get('/wizard/3', (req, res, next) => {
 })
 
 router.post('/wizard', (req, res, next) => {
-  res.render('wizard');
+  res.redirect('/wizard/2');
+})
+
+router.post('/wizard/2', (req, res, next) => {
+  res.redirect('/wizard/3');
+})
+
+router.post('/wizard/3', (req, res, next) => {
+  res.render('coffeeResults');
 })
 
 module.exports = router;
